6dbaeb1ae5083a5952a3e3ca58278c8f1e161671
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-06-01 21:06:32 +0600

    tab

--- Africa/App/MainView.swift
+++ Africa/App/MainView.swift
@@ -13,6 +13,22 @@ struct MainView: View {
             ContentView()
                 .tabItem {
                     Image(systemName: "square.grid.2x2")
+                    Text("Browse")
+                }
+            VideoListView()
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+            MapView()
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Locations")
+                }
+            GalleryView()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Gallery")
                 }
         }
         
