16413aaa89266a277a6e0a1f36eab97f99e05149
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-06-01 15:21:08 +0600

    Initial commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Shishir_Mac on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
