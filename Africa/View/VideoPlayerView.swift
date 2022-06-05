2069b0052c32aada49b96a2e8b5df52d84a5668a
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-06-05 13:34:06 +0600

    VideoListView

--- Africa/View/VideoPlayerView.swift
+++ Africa/View/VideoPlayerView.swift
@@ -10,13 +10,27 @@ import AVKit
 
 struct VideoPlayerView: View {
     //MARK: - Properties
-    //let video: Video
+    var videoSelected: String
+    var videoTitle: String
     
     
     //MARK: - Body
     var body: some View {
-        VideoPlayer(player: AVPlayer(url: Bundle.main.url(forResource: "cheetah", withExtension: "mp4")!))
-        
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")){
+                
+            }
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8), alignment: .topLeading
+            )
+        }
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle, displayMode: .inline)
     }
 }
 
@@ -26,8 +40,10 @@ struct VideoPlayerView_Previews: PreviewProvider {
     //static let videos:[Video] = Bundle.main.decode("videos.json")
     
     static var previews: some View {
-        VideoPlayerView()
-            .previewLayout(.sizeThatFits)
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion" )
+                .previewLayout(.sizeThatFits)
             .padding()
+        }
     }
 }
